be15264388e43f4caf61084b70c92c35736fe6fb
Dmytro Minyaylo <dimon9660@gmail.com>
2025-05-28 21:18:45 +0300

    configure app metadata and register models in admin interface

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from airport.models import (
+    Airport,
+    Route,
+    AirplaneType,
+    Airplane,
+    Crew,
+    Flight,
+    Order,
+    Ticket,
+)
 
-# Register your models here.
+admin.site.unregister(Group)
+
+admin.site.register(Airport)
+admin.site.register(Route)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Crew)
+admin.site.register(Flight)
+admin.site.register(Order)
+admin.site.register(Ticket)
