9930202738e0a3f00194c0e3cb86546910609860
Dmytro Minyaylo <dimon9660@gmail.com>
2025-05-30 18:38:32 +0300

    update MEDIA_ROOT

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "/files/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
